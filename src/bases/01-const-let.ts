29a6de5d36e1dfa4d8052f6da8e169423620ea15
Sara Lozano <saloz2001@gmail.com>
2025-02-11 15:17:14 +0100

    Constantes, variables y objetos

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+let urbs: string = 'Valencia';
+urbs = 'Madrid';
+
+const aetas: number = 24;
+
+const nomen = 'Tony';
+const cognomen = 'Stark';
+
+export const plenumNomen: string = `${ nomen } ${ cognomen } tiene ${ aetas * 2 } años y vive en ${ urbs }`;
+
+console.log(plenumNomen);
